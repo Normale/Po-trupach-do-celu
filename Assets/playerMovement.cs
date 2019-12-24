c28eb5024b6c7c0248af9daf416237633ba5443e
Normal <bartek.dybcio1@gmail.com>
2019-12-25 00:11:24 +0100

    Initial commit

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider2D;
+    private float speed = 5, jumpspeed = 5;
+    
+    bool jump = false;
+    private bool isMoving;
+    private bool isJumpPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    private void Awake()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+
+    }
+
+    private void Update()
+    {
+        isJumpPressed = Input.GetButtonDown("Jump");
+    }
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (isJumpPressed)
+        {
+            Debug.Log("jump");
+           rb.velocity = Vector2.up * jumpspeed;
+        }
+
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+        //horizontalMove = Input.GetAxis("Horizontal") * speed;
+
+        //rb.velocity.x = horizontalMove * Time.fixedDeltaTime;
+        
+
+    }
+   // private bool isGrounded()
+    
+
+    
+}
