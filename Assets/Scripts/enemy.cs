9faee20cb2d14738d68e217872594f369c70f256
Bartłomiej Dybcio <b.dybcio@stud.elka.pw.edu.pl>
2020-02-20 23:05:16 +0100

    Add enemy class

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+    BoxCollider2D boxCollider2D;
+    [Header("Enemy parameters")]
+    public float speed = 1;
+    int direction = 1;
+    float timer = 0;
+    public float changeTime = 4;
+    void Start()
+    {
+
+        rb = transform.GetComponent<Rigidbody2D>();
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            timer += Time.deltaTime;
+            if (timer > changeTime)
+            {
+                direction = direction == 1 ? -1 : 1;
+                speed = speed >= 20 ? 20 : (speed + 0.5f);
+                timer = 0f;
+            }
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(direction * speed, 0);
+    }
+}
